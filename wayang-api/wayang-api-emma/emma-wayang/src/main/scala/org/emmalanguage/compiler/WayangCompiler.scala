028d3abb9008000bc6333c9dfbe73d519269d48a
Gabor Gevay <ggab90@gmail.com>
2021-10-04 18:53:57 +0200

    Add SpecializeLoops to the Wayang transformations list  TODO: check that config option

--- wayang-api/wayang-api-emma/emma-wayang/src/main/scala/org/emmalanguage/compiler/WayangCompiler.scala
+++ wayang-api/wayang-api-emma/emma-wayang/src/main/scala/org/emmalanguage/compiler/WayangCompiler.scala
@@ -37,7 +37,7 @@ trait WayangCompiler extends Compiler
     Core.lift,
     // optimizations
     Core.cse iff "emma.compiler.opt.cse" is true,
-    //FlinkOptimizations.specializeLoops iff "emma.compiler.flink.native-its" is true,
+    WayangOptimizations.specializeLoops,// iff "emma.compiler.wayang.native-its" is true,
     Optimizations.foldFusion iff "emma.compiler.opt.fold-fusion" is true,
     Optimizations.addCacheCalls iff "emma.compiler.opt.auto-cache" is true,
     // backend
